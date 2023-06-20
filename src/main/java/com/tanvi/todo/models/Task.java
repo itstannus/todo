131eb89981ed7a1925bf4c5a45b616f61980a3f8
Tanvi Shinde <tanvishinde92@gmail.com>
2023-06-20 15:27:32 +0530

    Todo list Rest APIs using Springboot and MySQL

---
+++ src/main/java/com/tanvi/todo/models/Task.java
@@ -0,0 +1,42 @@
+package com.tanvi.todo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // this is the primary key which will be auto generated
+    private Long id;
+    private String task;
+    private boolean completed;
+
+    public Task() {
+    }
+
+    public Task(String task, boolean completed) {
+        this.task = task;
+        this.completed = completed;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTask() {
+        return task;
+    }
+    public void setTask(String task) {
+        this.task = task;
+    }
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
